1c748d6059b952969462aeac03e19077f3e96aff
Alexander Vitchinov <vitalex92@yandex.ru>
2020-12-11 23:59:43 +0300

    creating current user global context

--- src/components/App.js
+++ src/components/App.js
@@ -5,6 +5,7 @@ import Footer from "./Footer";
 import PopupWithForm from "./PopupWithForm";
 import ImagePopup from "./ImagePopup";
 import api from "../utils/api";
+import { CurrentUserContext } from "../contexts/currentUserContext";
 
 function App() {
   const [isEditAvatarPopupOpen, setIsEditAvatarPopupOpen] = React.useState(
@@ -15,7 +16,7 @@ function App() {
   );
   const [isAddPlacePopupOpen, setIsAddPlacePopupOpen] = React.useState(false);
   const [selectedCard, setSelectedCard] = React.useState(null);
-  const [currentUser, setCurrentUser] = React.useState();
+  const [currentUser, setCurrentUser] = React.useState("");
 
   const onEditAvatar = () => {
     setIsEditAvatarPopupOpen(true);
@@ -50,132 +51,134 @@ function App() {
 
   return (
     <div className="App">
-      <div className="page">
-        <Header />
-        <Main
-          onEditAvatar={onEditAvatar}
-          onEditProfile={onEditProfile}
-          onAddPlace={onAddPlace}
-          onCardClick={handleCardClick}
-        />
-        <Footer />
-        <PopupWithForm
-          title="Обновить аватар"
-          name="editAvatar"
-          modifier="content_text popup__container_update"
-          isOpened={isEditAvatarPopupOpen}
-          buttonTextContent="Сохранить"
-          children={
-            <section className="popup__form-section">
-              <input
-                type="url"
-                required
-                className="popup__input popup__input-update"
-                id="edit-ava-popup"
-                name="update"
-                placeholder="Обновимся?"
-              />
-              <span
-                className="popup__input_error"
-                id="edit-ava-popup-error"
-              ></span>
-            </section>
-          }
-          onClose={closeAllPopups}
-        />
-        <PopupWithForm
-          title="Редактировать профиль"
-          name="editProfile"
-          modifier="content_text"
-          isOpened={isEditProfilePopupOpen}
-          buttonTextContent="Сохранить"
-          children={
-            <>
-              <section className="popup__form-section">
-                <input
-                  type="text"
-                  required
-                  className="popup__input popup__input_name"
-                  id="edit-name-popup"
-                  minLength="2"
-                  maxLength="40"
-                  name="name"
-                  placeholder="Имя"
-                />
-                <span
-                  className="popup__input_error"
-                  id="edit-name-popup-error"
-                ></span>
-              </section>
-              <section className="popup__form-section">
-                <input
-                  type="text"
-                  required
-                  className="popup__input popup__input_job"
-                  id="edit-job-popup"
-                  minLength="2"
-                  maxLength="200"
-                  name="about"
-                  placeholder="Профессия"
-                />
-                <span
-                  className="popup__input_error"
-                  id="edit-job-popup-error"
-                ></span>
-              </section>
-            </>
-          }
-          onClose={closeAllPopups}
-        />
-        <PopupWithForm
-          title="Новое место"
-          name="addNewPlace"
-          modifier="content_text"
-          isOpened={isAddPlacePopupOpen}
-          buttonTextContent="Добавить"
-          children={
-            <>
-              <section className="popup__form-section">
-                <input
-                  type="text"
-                  required
-                  className="popup__input popup__input_title"
-                  id="create-title-popup"
-                  minLength="2"
-                  maxLength="30"
-                  name="name"
-                  placeholder="Название"
-                />
-                <span
-                  className="popup__input_error"
-                  id="create-title-popup-error"
-                ></span>
-              </section>
+      <CurrentUserContext.Provider value={currentUser}>
+        <div className="page">
+          <Header />
+          <Main
+            onEditAvatar={onEditAvatar}
+            onEditProfile={onEditProfile}
+            onAddPlace={onAddPlace}
+            onCardClick={handleCardClick}
+          />
+          <Footer />
+          <PopupWithForm
+            title="Обновить аватар"
+            name="editAvatar"
+            modifier="content_text popup__container_update"
+            isOpened={isEditAvatarPopupOpen}
+            buttonTextContent="Сохранить"
+            children={
               <section className="popup__form-section">
                 <input
                   type="url"
                   required
-                  className="popup__input popup__input_link"
-                  id="create-link-popup"
-                  name="link"
-                  placeholder="Ссылка на картинку"
+                  className="popup__input popup__input-update"
+                  id="edit-ava-popup"
+                  name="update"
+                  placeholder="Обновимся?"
                 />
                 <span
                   className="popup__input_error"
-                  id="create-link-popup-error"
+                  id="edit-ava-popup-error"
                 ></span>
               </section>
-            </>
-          }
-          onClose={closeAllPopups}
-        />
-        <ImagePopup
-          card={selectedCard || {}}
-          isOpened={selectedCard && "popup_is-opened"}
-          onCardClick={handleCardClick}
-          onClose={closeAllPopups}
-        />
-      </div>
+            }
+            onClose={closeAllPopups}
+          />
+          <PopupWithForm
+            title="Редактировать профиль"
+            name="editProfile"
+            modifier="content_text"
+            isOpened={isEditProfilePopupOpen}
+            buttonTextContent="Сохранить"
+            children={
+              <>
+                <section className="popup__form-section">
+                  <input
+                    type="text"
+                    required
+                    className="popup__input popup__input_name"
+                    id="edit-name-popup"
+                    minLength="2"
+                    maxLength="40"
+                    name="name"
+                    placeholder="Имя"
+                  />
+                  <span
+                    className="popup__input_error"
+                    id="edit-name-popup-error"
+                  ></span>
+                </section>
+                <section className="popup__form-section">
+                  <input
+                    type="text"
+                    required
+                    className="popup__input popup__input_job"
+                    id="edit-job-popup"
+                    minLength="2"
+                    maxLength="200"
+                    name="about"
+                    placeholder="Профессия"
+                  />
+                  <span
+                    className="popup__input_error"
+                    id="edit-job-popup-error"
+                  ></span>
+                </section>
+              </>
+            }
+            onClose={closeAllPopups}
+          />
+          <PopupWithForm
+            title="Новое место"
+            name="addNewPlace"
+            modifier="content_text"
+            isOpened={isAddPlacePopupOpen}
+            buttonTextContent="Добавить"
+            children={
+              <>
+                <section className="popup__form-section">
+                  <input
+                    type="text"
+                    required
+                    className="popup__input popup__input_title"
+                    id="create-title-popup"
+                    minLength="2"
+                    maxLength="30"
+                    name="name"
+                    placeholder="Название"
+                  />
+                  <span
+                    className="popup__input_error"
+                    id="create-title-popup-error"
+                  ></span>
+                </section>
+                <section className="popup__form-section">
+                  <input
+                    type="url"
+                    required
+                    className="popup__input popup__input_link"
+                    id="create-link-popup"
+                    name="link"
+                    placeholder="Ссылка на картинку"
+                  />
+                  <span
+                    className="popup__input_error"
+                    id="create-link-popup-error"
+                  ></span>
+                </section>
+              </>
+            }
+            onClose={closeAllPopups}
+          />
+          <ImagePopup
+            card={selectedCard || {}}
+            isOpened={selectedCard && "popup_is-opened"}
+            onCardClick={handleCardClick}
+            onClose={closeAllPopups}
+          />
+        </div>
+      </CurrentUserContext.Provider>
     </div>
   );
 }
--- src/components/Main.js
+++ src/components/Main.js
@@ -2,19 +2,17 @@ import React from "react";
 import { useEffect } from "react";
 import api from "../utils/api";
 import Card from "./Card";
+import { CurrentUserContext } from "../contexts/currentUserContext";
 
 function Main({ onEditAvatar, onEditProfile, onAddPlace, onCardClick }) {
-  const [userName, setUserName] = React.useState("");
-  const [userDescription, setUserDescription] = React.useState("");
-  const [userAvatar, setUserAvatar] = React.useState("");
   const [cards, setCards] = React.useState([]);
-  // try on API
+
+  const currentUser = React.useContext(CurrentUserContext);
+
   useEffect(() => {
-    Promise.all([api.getUserInfo(), api.getCards()])
-      .then(([response, data]) => {
-        setUserName(response.name);
-        setUserDescription(response.about);
-        setUserAvatar(response.avatar);
+    api
+      .getCards()
+      .then((data) => {
         data.forEach((item) => {
           return {
             id: item._id,
@@ -33,7 +31,7 @@ function Main({ onEditAvatar, onEditProfile, onAddPlace, onCardClick }) {
     <main className="content">
       <section className="profile">
         <img
-          src={userAvatar}
+          src={currentUser.avatar}
           className="profile__image"
           name="avatar"
           alt="Аватарка"
@@ -41,12 +39,12 @@ function Main({ onEditAvatar, onEditProfile, onAddPlace, onCardClick }) {
         <button className="profile__edit" onClick={onEditAvatar}></button>
 
         <div className="profile__info">
-          <h2 className="profile__info-name">{userName}</h2>
+          <h2 className="profile__info-name">{currentUser.name}</h2>
           <button
             className="profile__info-edit"
             onClick={onEditProfile}
           ></button>
-          <p className="profile__info-job">{userDescription}</p>
+          <p className="profile__info-job">{currentUser.about}</p>
         </div>
         <button className="profile__add-button" onClick={onAddPlace}></button>
       </section>
